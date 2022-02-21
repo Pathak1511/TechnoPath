86923e7df61e1df7a073cbaee98059e8494edceb
Hritik Pathak <hritik1511@gmail.com>
2022-02-21 18:45:56 +0530

    Modified

--- server.js
+++ server.js
@@ -29,13 +29,14 @@ mongoose
   .then(() => console.log('DB connection successfull'));
 
 // const port = 5500 || process.env.PORT;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 var server = http.createServer((req, res) => {
   //your stuff
 });
 
 //${port} in ${process.env.NODE_ENV}
-server.listen(process.env.PORT || 5500, () => {
+server.listen(process.env.PORT || 5500, server_host, () => {
   // console.log('App running on port ');
 });
 
