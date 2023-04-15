31f171c3edc490b406f085573cfc518258cccc46
Pathak1511 <hritik1511@gmail.com>
2023-04-15 16:27:28 +0530

    modified gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,18 @@
 const gulp = require('gulp');
 
 // Include plugins
-const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 
 // Compile Sass
-gulp.task('sass', function () {
+gulp.task('css', function () {
   return gulp
     .src('public/CSS/**/*.css')
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(autoprefixer())
+    .pipe(cleanCSS())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('dist/css'));
 });
@@ -29,10 +29,10 @@ gulp.task('scripts', function () {
 });
 
 // Watch files for changes
-gulp.task('watch', function () {
-  gulp.watch('public/CSS/**/*.css', gulp.series('sass'));
-  gulp.watch('public/JS/**/*.js', gulp.series('scripts'));
-});
+// gulp.task('watch', function () {
+//   gulp.watch('public/CSS/**/*.css', gulp.series('css'));
+//   gulp.watch('public/JS/**/*.js', gulp.series('scripts'));
+// });
 
 // Default task
-gulp.task('default', gulp.parallel('sass', 'scripts', 'watch'));
+gulp.task('default', gulp.parallel('css', 'scripts'));
